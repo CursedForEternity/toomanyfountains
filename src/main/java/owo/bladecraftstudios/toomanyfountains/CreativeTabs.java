6d29a5073c329a0260ca8851442abd0ff18b58c2
IndigoM341 <indigomaxwell341@gmail.com>
2026-06-07 03:39:09 -0700

    Update CreativeTabs.java

--- src/main/java/owo/bladecraftstudios/toomanyfountains/CreativeTabs.java
+++ src/main/java/owo/bladecraftstudios/toomanyfountains/CreativeTabs.java
@@ -22,8 +22,11 @@ public class CreativeTabs {
         output.accept(DWItems.JELLITE.get());
         output.accept(DWBlocks.JELLITE_ORE.get());
         output.accept(DWBlocks.MUSHITE.get());
+        output.accept(DWBlocks.FUNGITE.get());
         output.accept(DWBlocks.BLIGHTSHROOM_PLANKS.get());
         output.accept(DWBlocks.MUSHSNOW.get());
+        output.accept(DWBlocks.FUNGAL_MUSHITE.get());
+        output.accept(DWBlocks.FUNGAL_FUNGITE.get());
     }).build());
     public static final RegistryObject<CreativeModeTab> ICE_TAB = CREATIVE_TABS.register("ice_dw", () -> CreativeModeTab.builder().icon(() -> new ItemStack(DWBlocks.ICESALT.get())).title(Component.translatable("A Cold Place")).displayItems((itemDisplayParameters, output) -> {
         output.accept(DWBlocks.ICESALT.get());
