3f1fa2bc8693e503aad637e6ee8fd09b56112d5d
SashaDEM2010 <130908168+SashaDEM2010@users.noreply.github.com>
2026-07-27 10:59:27 +0300

    eeh?

--- src/main/java/owo/bladecraftstudios/toomanyfountains/spellsystem/ItemSpellUtil.java
+++ src/main/java/owo/bladecraftstudios/toomanyfountains/spellsystem/ItemSpellUtil.java
@@ -1,4 +1,16 @@
 package owo.bladecraftstudios.toomanyfountains.spellsystem;
 
+import net.minecraft.world.item.Item;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod;
+import owo.bladecraftstudios.toomanyfountains.TooManyFountains;
+
+import java.util.Set;
+
+@Mod.EventBusSubscriber(modid = TooManyFountains.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ItemSpellUtil {
+
+    public static void onRightClick(PlayerInteractEvent.RightClickItem event) {
+
+    }
 }
