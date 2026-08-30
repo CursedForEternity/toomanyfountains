f8753e673ea82d36fd7774d2afa653b9b0bd4a08
SashaDEM2010 <demchuksasha4@gmail.com>
2026-08-30 16:53:52 +0200

    stuff for menus

---
+++ src/main/java/owo/bladecraftstudios/toomanyfountains/server/registries/DWMenu.java
@@ -0,0 +1,15 @@
+package owo.bladecraftstudios.toomanyfountains.server.registries;
+
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import owo.bladecraftstudios.toomanyfountains.TooManyFountains;
+import owo.bladecraftstudios.toomanyfountains.client.renderer.menu.RootsCraftingTableMenu;
+
+public class DWMenu {
+    public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(ForgeRegistries.MENU_TYPES, TooManyFountains.MODID);
+    public static final RegistryObject<MenuType<RootsCraftingTableMenu>> ROOTS_CRAFTING_TABLE = MENUS.register("roots_crafting_table_menu",
+            () -> IForgeMenuType.create((windowId, inv, data) -> new RootsCraftingTableMenu(windowId, inv)));
+}
